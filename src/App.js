ac01602766cd5a0c16377a08aa115872aa724e25
Zarry Myles <mahanth2709@gmail.com>
2022-07-23 01:51:34 +0530

    Added Custom Cursor

--- src/App.js
+++ src/App.js
@@ -1,9 +1,53 @@
+import React, {useState, useEffect} from 'react';
+import {motion} from 'framer-motion';
 import './App.css';
 
 function App() {
+  const [mousePos, setMousePos] = useState({
+    x: 0,
+    y: 0
+  })
+  const [cursorVariant, setcursorVariant] = useState("default")
+
+  useEffect(()=> {
+    const mouseMove = e => {
+      setMousePos({
+        x: e.clientX,
+        y: e.clientY
+      })
+    }
+    window.addEventListener("mousemove", mouseMove)
+
+    return () =>{
+      window.removeEventListener("mousemove", mouseMove)
+    }
+
+  }, [])
+
+  const variants = {
+    default: {
+      x: mousePos.x - 14,
+      y: mousePos.y - 14,
+      bounce: 0
+    },
+    text: {
+      height: 80,
+      width: 80,
+      x: mousePos.x - 40,
+      y: mousePos.y - 40,
+      backgroundColor: '#d1d5db',
+      mixBlendMode: 'difference',
+      bounce: 0,
+    }
+  }
+
+  const textEnter = () => setcursorVariant("text")
+  const textLeave = () => setcursorVariant("default")
+
   return (
-    <div className="bg-black h-screen flex items-center justify-center">
-      <h1 className='text-white font-mono text-xl'>Sam Portfolio</h1>
+    <div className="bg-gray-300 h-screen flex items-center justify-center">
+      <h1 onMouseEnter={textEnter} onMouseLeave={textLeave} className='text-black text-9xl'>Sam Port</h1>
+      <motion.div className='cursor' variants={variants} animate={cursorVariant} />
     </div>
   );
 }
