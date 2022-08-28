225199cf82e814f1810ba30440caafea39e803d7
Zarry Myles <mahanth2709@gmail.com>
2022-08-28 13:48:46 +0530

    Loader changes

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -5,8 +5,18 @@ const Loader = () => {
 
   return (
     <div className="flex z-10 flex-col p-6 justify-center text-white items-center bg-brand-darkGrey w-screen h-screen">
-      <div className="font-lato-light text-5xl">Shradha</div>
-      <div>progress</div>
+      <div className="font-lato-light text-4xl mb-4">Shradha</div>
+      <div className="w-full h-auto flex justify-center items-center">
+        <div
+          className="bg-gray-600"
+          style={{ width: "25%", height: "0.5px", borderRadius: "1px" }}
+        >
+          <div
+            className="bg-white loaderLine"
+            style={{ height: "1.5px", borderRadius: "10px" }}
+          ></div>
+        </div>
+      </div>
     </div>
   );
 };
